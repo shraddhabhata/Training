469992e0dfa23824a7cd48c98a86173dff5e1b66
Andrew Aponte <webdev87@gmail.com>
2015-09-10 12:33:43 -0400

    Added check for null objects

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -2,6 +2,11 @@ function checkAnswer(value) {
   var type = typeof eval(value),
       correct   = false;
 
+  // Even though null is a primitive type, it return 'object' with the typeof operator
+  if (value === 'null') {
+    type = 'null';
+  }
+
   switch (type) {
     case 'function':
     case 'object':
